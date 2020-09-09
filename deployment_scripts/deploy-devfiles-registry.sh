b0f35459594e6b284240c737e3dac6a596fe2334
Rashmi P Singh <rashmi.singh@in.ibm.com>
2020-09-09 17:43:06 +0530

    Update deploy-devfiles-registry.sh

--- deployment_scripts/deploy-devfiles-registry.sh
+++ deployment_scripts/deploy-devfiles-registry.sh
@@ -5,28 +5,36 @@ NAMESPACE=$2
 
 kubectl version --short
 
-echo "\n\n========== REPLACING THE LATEST BUILD TAG IN CRD OF ${ENV} ENVIRONMENT =========="
+echo "========== REPLACING THE LATEST BUILD TAG IN CRD OF ${ENV} ENVIRONMENT =========="
 
-newtag=`cat ${ENV}/image-tags/devfiles_registry_latest.txt`
+#fetch the currently deployed build
 currentimage=`kubectl get deployment devfile-registry -o=jsonpath='{$.spec.template.spec.containers[:1].image}' -n ${NAMESPACE}`
 currenttag=${currentimage#*:}
+
+#fetch the new build to be deployed
+newtag=`cat ${ENV}/image-tags/devfiles_registry_latest.txt`
+
+#updating the crd.yaml and devfiles_registry_previous.txt
 echo "Image to be deployed in devfiles registry is : us.icr.io/apihub-cr/che-devfile-registry:${newtag}"
 sed -i "s/che-devfile-registry:.*'/che-devfile-registry:${newtag}'/" "${ENV}/yaml/crd.yaml"
 true > ${ENV}/image-tags/devfiles_registry_previous.txt
 echo ${currenttag} > ${ENV}/image-tags/devfiles_registry_previous.txt
 
-echo "\n\n========== APPLYING THE BELOW CRD FILE =========="
+echo "========== APPLYING THE BELOW CRD FILE =========="
+#print the updated crd.yaml content before applying
 cat ${ENV}/yaml/crd.yaml
-echo "\n\n"
-kubectl apply -f ${ENV}/yaml/crd.yaml -n ${NAMESPACE}
+echo ""
+
+#applying the crd.yaml and reading the output of the command
 OUTPUT=`kubectl apply -f ${ENV}/yaml/crd.yaml -n ${NAMESPACE}`
-checluster.org.eclipse.che/playground-dev-cluster configured
+
+#if pass,checkin the crd.yaml and devfiles_registry_previous.txt changes else exit with failure
 if [[ "${OUTPUT}" == "checluster.org.eclipse.che/"*" configured" ]] || [[ "${OUTPUT}" == "checluster.org.eclipse.che/"*" unchanged" ]]
         then
 			git config --global user.email 'apihub@in.ibm.com'
 			git config --global user.name 'apihub'
 			git add --all
-			git commit -m 'updating devfiles tag in crd of ${ENV} environment'
+			git commit -m "updating devfiles tag in crd of ${ENV} environment"
 			git push https://${MYUSER}:${MYTOKEN}@github.ibm.com/ibm-api-marketplace/playground-resources.git HEAD:reorg -f
 		else
 		    echo "Failed to deploy new build of devfiles registry"
