8f8b4a5e3906f4a9ae4827285afe197f275ff96e
RashmiSingh <RashmiSingh@9.85.108.51>
2020-09-09 17:10:42 +0530

    debug

--- deployment_scripts/deploy-devfiles-registry.sh
+++ deployment_scripts/deploy-devfiles-registry.sh
@@ -18,6 +18,7 @@ echo ${currenttag} > ${ENV}/image-tags/devfiles_registry_previous.txt
 echo "\n\n========== APPLYING THE BELOW CRD FILE =========="
 cat ${ENV}/yaml/crd.yaml
 echo "\n\n"
+kubectl apply -f ${ENV}/yaml/crd.yaml -n ${NAMESPACE}
 OUTPUT=`kubectl apply -f ${ENV}/yaml/crd.yaml -n ${NAMESPACE}`
 if [[ "${OUTPUT}" == "checluster.org.eclipse.che/${NAMESPACE} configured" ]] || [[ "${OUTPUT}" == "checluster.org.eclipse.che/${NAMESPACE} unchanged" ]]
         then
