ffd5f75092c6dd6b618f4fcd82afb43c74568a23
RashmiSingh <RashmiSingh@9.77.205.182>
2020-09-10 13:55:19 +0530

    adding new script

---
+++ scripts/deploy-all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ENV=$1
+NAMESPACE=$2
+
+kubectl version --short
+
+YAMLFILES=/${ENV}/yaml/*
+for f in $YAMLFILES
+do
+  echo "Processing $f file..."
+  OUTPUT=`kubectl apply -f ${ENV}/yaml/$f -n ${NAMESPACE}`
+  cat ${OUTPUT}
+  if [[ "${OUTPUT}" == *"configured" ]] || [[ "${OUTPUT}" == *"unchanged" ]]
+        then
+			echo "Successfully applied the $f file"
+		else
+		    echo "Failed to apply the $f file"
+			exit 1
+  fi
+  cat $f
+done
