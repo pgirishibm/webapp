0959618102f9d4bebc49127ed7a127ca549264b5
Rashmi P Singh <rashmi.singh@in.ibm.com>
2020-09-17 17:28:31 +0530

    Update deploy-all.sh

--- scripts/deploy-all.sh
+++ scripts/deploy-all.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
 ENV=$1
-NAMESPACE=$2
 
 kubectl version --short
+
+declare -A dict
+while IFS='=' read -r key value; do
+    dict[$key]=$value
+done < ${WORKSPACE}/${ENV}/deployfile.txt
+
+echo "${dict[$f]}"
 
 YAMLFILES=${ENV}/yaml/*.yaml
 for f in $YAMLFILES
 do
-  echo "Processing $f file..."
-  OUTPUT=`kubectl apply -f $f -n ${NAMESPACE}`
+  echo " Deploying $f file in ${dict[$f]} namespace ..."
+  #OUTPUT=`kubectl apply -f $f -n ${dict[$f]}`
+  OUTPUT=" sample configured"
   echo ${OUTPUT}
   if [[ "${OUTPUT}" == *"configured" ]] || [[ "${OUTPUT}" == *"unchanged" ]]
         then
-			echo "Successfully applied the $f file"
+			echo "Successfully applied the $f file in ${dict[$f]} namespace"
 		else
 		    echo "Failed to apply the $f file"
 			exit 1
